49fde003f0997bcbc0f33bed1e1ffff7484ad1a0
sanroman-24 <angelfsanroman@gmail.com>
2024-02-21 11:46:05 +0100

    Add proliferation and SLC19A1 survival analyses

---
+++ analysis/scripts/surv_9p_prolif.R
@@ -0,0 +1,110 @@
+# Calculate prognostic association of proliferation + 9p loss in TRACERx Renal
+
+rm(list = ls(all = TRUE))
+
+
+# LIBRARIES ---------------------------------------------------------------
+
+
+library(tidyverse)
+library(survminer)
+library(survival)
+library(ggthemes)
+
+# PATHS -------------------------------------------------------------------
+
+BASE <- here::here()
+DATA_DIR <- file.path(BASE, "data", "raw")
+META_DIR <- file.path(BASE, "data", "meta")
+OUT_DIR <- file.path(BASE, "analysis", "outputs")
+FIG_DIR <- file.path(BASE, "analysis", "figures")
+ANNOTATION_PATH <- file.path(META_DIR, "tx_annotation.tsv")
+CLINICAL_ANNOTATION_PATH <- file.path(META_DIR, "TRACERx_s1_1_clinical.txt")
+SSGSEA_PATH <- file.path(BASE, "data", "processed", "tx_ssGSEA.tsv")
+HALLMARK_GROUPS_PATH <- file.path(META_DIR, "martinez_ruiz_2023_hallmark_gs_groups.txt")
+
+source(file.path(BASE, "src", "plotting_theme.R"))
+source(file.path(BASE, "src", "plotting_functions.R"))
+# LOAD DATA ---------------------------------------------------------------
+annotation <- read_delim(ANNOTATION_PATH, delim = "\t")
+gene_groups <- read_delim(HALLMARK_GROUPS_PATH)
+ssgsea_scores <- read_delim(SSGSEA_PATH)
+# clinical annotation of the TRACERx Renal cohort
+clinical_data <- read_delim(CLINICAL_ANNOTATION_PATH, delim = "\t")
+colnames(clinical_data) <- clinical_data[1, ]
+clinical_data <- clinical_data[-1, ]
+annotation <- annotation[annotation$sample != "K207-R4", ]
+
+# only primary samples
+annotation <- annotation[annotation$type_collapsed == "PRIMARY", ]
+# get a "proliferation" score
+annotation <- merge(annotation, ssgsea_scores, "sample")
+
+gene_groups$pathway <- paste0("HALLMARK_", str_to_upper(gene_groups$Hallmark))
+pf_paths <- gene_groups$pathway[gene_groups$Functional_group == "proliferation"]
+
+scores <- annotation %>%
+    dplyr::select(Patient, pf_paths) %>%
+    group_by(Patient) %>%
+    summarise_all(max) %>%
+    {
+        cbind(.[, 1], data.frame(proliferation = rowMeans(.[, -1])))
+    }
+
+# Add information on status 9p loss
+loss_9p <- annotation %>%
+    group_by(Patient) %>%
+    summarise(loss_9p = sum(loss_9p)) %>%
+    mutate(loss_9p = ifelse(loss_9p > 0, 1, 0))
+
+scores <- merge(scores, loss_9p)
+
+scores$Subject <- scores$Patient
+scores <- merge(scores, clinical_data, by = "Subject") %>%
+    mutate(pfs = ifelse(`PFS (months)` == "-", 0, 1)) %>%
+    mutate(pfs_time = as.numeric(ifelse(`PFS (months)` == "-",
+        `Total follow up (months)`, `PFS (months)`
+    ))) %>%
+    mutate(os = ifelse(Outcome == "Death", 1, 0)) %>%
+    mutate(os_time = as.numeric(`Total follow up (months)`))
+
+prol_thresh <- quantile(scores$proliferation, .75)
+scores$high_proliferation <- scores$proliferation > prol_thresh
+scores <- scores %>% mutate(group = case_when(
+    high_proliferation & loss_9p ~ "9p loss + high proliferation",
+    !high_proliferation & loss_9p ~ "9p loss + low proliferation",
+    high_proliferation & !loss_9p ~ "9p WT + high proliferation",
+    !high_proliferation & !loss_9p ~ "9p WT + low proliferation",
+))
+
+
+### SURVIVAL ANALYSIS
+# PFS
+plt <- "lancet"
+
+km_fit <- survfit(Surv(pfs_time, pfs) ~ group,
+    data = scores
+)
+
+km_plot <- plot_km(scores, km_fit, plt = plt)
+
+save_baseplot(km_plot, file.path(PLOT_DIR, "SupFigX_9p_loss_prolif_pfs"),
+    h = 90, w = 70
+)
+
+# OS
+km_fit <- survfit(Surv(os_time, os) ~ group,
+    data = scores
+)
+
+km_plot <- plot_km(scores, km_fit, plt = plt)
+
+save_baseplot(km_plot, file.path(PLOT_DIR, "SupFigX_9p_loss_prolif_os"),
+    h = 90, w = 70
+)
+
+
+# Check upon correction by stage
+scores$stage_simple <- ifelse(scores$`Overall Stage` %in% c("I", "II"), "I-II", "III-IV")
+coxph(Surv(pfs_time, pfs) ~ loss_9p:high_proliferation + stage_simple, data = scores)
+coxph(Surv(os_time, pfs) ~ proliferation + stage_simple, data = scores[scores$loss_9p > 0, ])
---
+++ analysis/scripts/surv_pan_cancer_slc19a1.R
@@ -0,0 +1,159 @@
+# Survival analysis ENPP1 and SLC19A1 pan-cancer
+
+rm(list = ls(all = TRUE))
+
+
+# LIBRARIES ---------------------------------------------------------------
+
+library(tidyverse)
+library(DESeq2)
+library(survival)
+library(survminer)
+
+# PATHS -------------------------------------------------------------------
+
+BASE <- here::here()
+DATA_DIR <- file.path(BASE, "data", "raw")
+META_DIR <- file.path(BASE, "data", "meta")
+OUT_DIR <- file.path(BASE, "analysis", "outputs")
+FIG_DIR <- file.path(BASE, "analysis", "figures")
+CLINICAL_PATH <- file.path(META_DIR, "allClinicalTCGA.txt")
+
+
+source(file.path(BASE, "src", "plotting_theme.R"))
+source(file.path(BASE, "src", "plotting_functions.R"))
+# SURVIVAL ANALYSIS --------------------------------------------------------
+
+# load clinical data
+clin_anno <- read_delim(file = CLINICAL_PATH, delim = "\t")
+clin_anno$PFI <- as.numeric(clin_anno$PFI)
+clin_anno$OS <- as.numeric(clin_anno$OS)
+clin_anno$PFI.time <- as.numeric(clin_anno$PFI.time)
+clin_anno$OS.time <- as.numeric(clin_anno$OS.time)
+
+# prepare to parse through all the RNA-Seq files and perform ttype survival analysis
+fps <- list.files(DATA_DIR, "RNASeq.RDS")
+hrPFSSurvivalLowHigh <- data.frame()
+hrOSSurvivalLowHigh <- data.frame()
+
+hrPFSSurvivalLowHighSLCENPP <- data.frame()
+hrOSSurvivalLowHighSLCENPP <- data.frame()
+
+hrPFSSurvivalSLC19A1 <- data.frame()
+hrOSSurvivalSLC19A1 <- data.frame()
+
+i <- 0
+for (fp in fps){
+  if (str_detect(fp, "\\w{4}_RNASeq.RDS")){
+    i <- i + 1
+    tumourType <- str_sub(fp, 1, 4)
+    fp <- file.path(DATA_DIR, fp)
+    dataset <- read_rds(fp)
+    # In TCGA dataset, consider only primary solid tumour samples
+    dataset <- dataset[ , dataset$definition == "Primary solid Tumor"]
+    dataset <- dataset[, !duplicated(dataset$patient)]
+    
+    # compute variance stabilizing transformation
+    counts <- dataset@assays@data$`HTSeq - Counts`
+    rownames(counts) <- dataset@rowRanges$external_gene_name
+    colnames(counts) <- dataset@colData$patient
+
+    rownames(dataset@colData) = dataset@colData$patient
+    
+    dds <- DESeqDataSetFromMatrix(countData = counts,
+                                  colData = dataset@colData, 
+                                  design = ~ patient)
+    
+    vst <- vst(dds, blind = TRUE)
+    
+    # prepare to merge to clinical data
+    # change data type to data frame and then put genes in columns and patient IDs in rows
+    SLC19A1_vst <- assay(vst[rownames(vst) %in% c("SLC19A1", "ENPP1"), ]) %>% 
+      as.data.frame() %>% t() %>% as.data.frame()
+    # only consider first 12 characters of patient ID as they are the way they are stored in the clinical annotation
+    rownames(SLC19A1_vst) <- str_sub(rownames(SLC19A1_vst), 1, 12)
+    # move rownames to a new variable to allow merging
+    SLC19A1_vst <- rownames_to_column(SLC19A1_vst, "bcr_patient_barcode")
+    # merge
+    clinical_SLC19A1 <- merge(SLC19A1_vst, clin_anno, by = "bcr_patient_barcode") 
+    
+    # define high and low SLC19A1 groups
+    SLC19A1_thresh <- quantile(clinical_SLC19A1$SLC19A1, 0.75)
+    clinical_SLC19A1 <- clinical_SLC19A1 %>% mutate(high_SLC19A1 = ifelse(SLC19A1 > SLC19A1_thresh, "high_SLC19A1", "low_SLC19A1"))
+    
+    # define high and low ENPP1 groups
+    ENPP1_thresh <- quantile(clinical_SLC19A1$ENPP1, 0.75)
+    clinical_SLC19A1 <- clinical_SLC19A1 %>% mutate(high_ENPP1 = ifelse(ENPP1 > ENPP1_thresh, "high_ENPP1", "low_ENPP1"))
+    
+    # define high and low SLCENPP groups
+    clinical_SLC19A1 <- clinical_SLC19A1 %>% mutate(high_SLCENPP = ifelse((high_ENPP1 == "high_ENPP1" & high_SLC19A1 == "high_SLC19A1"), 
+                                                    "high_SLCENPP1", "low_SLCENPP1"))
+    
+    # Cox univariate analysis between binary classification based on SLC19A1
+    coxUVABinary <- coxph(Surv(as.numeric(PFI.time), PFI) ~ high_SLC19A1,
+                          data=clinical_SLC19A1)
+    resultsBinaryCox <- data.frame(tumour = tumourType, CI2.5 = exp(confint(coxUVABinary)[1]), CI97.5 = exp(confint(coxUVABinary)[2]))
+    hrPFSSurvivalLowHigh <- rbind(hrPFSSurvivalLowHigh, resultsBinaryCox)
+    
+    coxUVABinary <- coxph(Surv(as.numeric(OS.time), OS) ~ high_SLC19A1,
+                          data=clinical_SLC19A1)
+    resultsBinaryCox <- data.frame(tumour = tumourType, CI2.5 = exp(confint(coxUVABinary)[1]), CI97.5 = exp(confint(coxUVABinary)[2]))
+    hrOSSurvivalLowHigh <- rbind(hrOSSurvivalLowHigh, resultsBinaryCox)
+    
+    # Cox univariate analysis between binary classification based on SLC19A1 & ENPP1
+    coxUVABinary <- coxph(Surv(as.numeric(PFI.time), PFI) ~ high_SLCENPP,
+                          data=clinical_SLC19A1)
+    resultsBinaryCox <- data.frame(tumour = tumourType, CI2.5 = exp(confint(coxUVABinary)[1]), CI97.5 = exp(confint(coxUVABinary)[2]))
+    hrPFSSurvivalLowHighSLCENPP <- rbind(hrPFSSurvivalLowHighSLCENPP, resultsBinaryCox)
+    
+    coxUVABinary <- coxph(Surv(as.numeric(OS.time), OS) ~ high_SLCENPP,
+                          data=clinical_SLC19A1)
+    resultsBinaryCox <- data.frame(tumour = tumourType, CI2.5 = exp(confint(coxUVABinary)[1]), CI97.5 = exp(confint(coxUVABinary)[2]))
+    hrOSSurvivalLowHighSLCENPP <- rbind(hrOSSurvivalLowHighSLCENPP, resultsBinaryCox)
+    
+    # Cox univariate analysis with continuous SLC19A1 expression
+    coxUVAContinuous <- coxph(Surv(as.numeric(PFI.time), PFI) ~ SLC19A1,
+                          data=clinical_SLC19A1)
+    resultsContinuousCox <- data.frame(tumour = tumourType, CI2.5 = exp(confint(coxUVAContinuous)[1]), CI97.5 = exp(confint(coxUVAContinuous)[2]))
+    hrPFSSurvivalSLC19A1 <- rbind(hrPFSSurvivalSLC19A1, resultsContinuousCox)
+    
+    coxUVAContinuous <- coxph(Surv(as.numeric(OS.time), OS) ~ SLC19A1,
+                          data=clinical_SLC19A1)
+    resultsContinuousCox <- data.frame(tumour = tumourType, CI2.5 = exp(confint(coxUVAContinuous)[1]), CI97.5 = exp(confint(coxUVAContinuous)[2]))
+    hrOSSurvivalSLC19A1 <- rbind(hrOSSurvivalSLC19A1, resultsContinuousCox)
+  }
+}
+
+p <- hrOSSurvivalSLC19A1 %>% mutate(CI50 = (CI2.5+CI97.5) / 2) %>%
+  mutate(col = ifelse(CI2.5 >= 1, "red", "grey")) %>%  
+  ggplot(aes(y = reorder(tumour, CI2.5), xmin = CI2.5, xmax = CI97.5, x = CI50, col = col)) + 
+  geom_vline(xintercept = 1, lty = "dashed") + 
+  scale_color_manual(values = c("lightgrey", "firebrick3")) + 
+  geom_pointrange() + 
+  theme_classic() + 
+  xlim(c(0, 5)) + 
+  theme(axis.line = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  theme(strip.background = element_rect(colour="white")) + 
+  theme(legend.position = "none") + 
+  labs(x = "Hazard Ratio", y = "") + 
+  scale_x_log10(breaks = c(0.1, 1, 10), limits = c(0.1, 10)) 
+
+# save plot
+save_ggplot(p, file.path(FIG_DIR, "SupFig_pancan_os_slc19a1"), w = 100, h = 100)
+
+# plot forest plot for PFS Hazard Ratio with SLC19A1 as continuous variable
+p <- hrPFSSurvivalSLC19A1 %>% mutate(CI50 = (CI2.5+CI97.5) / 2) %>%
+  mutate(col = ifelse(CI2.5 >= 1, "red", "grey")) %>%  
+  ggplot(aes(y = reorder(tumour, CI2.5), xmin = CI2.5, xmax = CI97.5, x = CI50, col = col)) + 
+  geom_vline(xintercept = 1, lty = "dashed") + 
+  scale_color_manual(values = c("lightgrey", "firebrick3")) + 
+  geom_pointrange() + 
+  theme_classic() + 
+  theme(axis.line = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  theme(strip.background = element_rect(colour="white")) + 
+  theme(legend.position = "none") + 
+  labs(x = "Hazard Ratio", y = "") + 
+  scale_x_log10(breaks = c(0.1, 1, 10), limits = c(0.1, 10)) 
+
+# save plot
+save_ggplot(p, file.path(FIG_DIR, "SupFig_pancan_pfs_slc19a1"), w = 100, h = 100)
