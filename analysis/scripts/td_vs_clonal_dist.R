6c7c54067010d3e2316722121e7b2db020b26a6d
sanroman-24 <angelfsanroman@gmail.com>
2024-06-18 10:32:21 +0100

    Added correction by differences in purity for Figure 2C

--- analysis/scripts/td_vs_clonal_dist.R
+++ analysis/scripts/td_vs_clonal_dist.R
@@ -15,6 +15,7 @@ META_DIR <- file.path(BASE, "data", "meta")
 FIG_DIR <- file.path(BASE, "analysis", "figures")
 TD_MAT_PATH <- file.path(BASE, "analysis", "outputs", "ITED_matrix.rds")
 CLONES_PATH <- file.path(META_DIR, "clones_annotation.txt")
+ANNOTATION_PATH <- file.path(BASE, "data", "meta", "tx_annotation.tsv")
 
 # FUNCTIONS ---------------------------------------------------------------
 source(file.path(BASE, "src", "plotting_theme.R"))
@@ -23,6 +24,12 @@ source(file.path(BASE, "src", "get_clonal_dist.R"))
 source(file.path(BASE, "src", "utils.R"))
 
 # LOAD DATA ---------------------------------------------------------------
+annotation <- read_delim(ANNOTATION_PATH)
+annotation$sample <- annotation$sample %>%
+  str_replace("K328R19", "K328-R19") %>%
+  str_replace("-", "_") %>%
+  str_replace("K328_T1", "K328_THR1") %>%
+  str_replace("K245_T1", "K245_THR1")
 
 td_mat <- readRDS(TD_MAT_PATH)
 colnames(td_mat) <- str_remove(colnames(td_mat), "^[RG]_") %>%
@@ -37,12 +44,12 @@ clones_annotation <- read_delim(CLONES_PATH, delim = "\t")
 clones_annotation <- clones_annotation[!is.na(clones_annotation$parent_clone), ]
 monoclonal_regions <- get_monoclonal_regions_df(clones_annotation)
 clones_annotation <- clones_annotation[clones_annotation$sample %in% monoclonal_regions$smp, ]
-
+clones_annotation <- merge(clones_annotation, annotation[c("purity", "sample")])
 # CALCULATE TRANSC DISTANCE BETWEEN SAMPLES -------------------------------
 
 clonal_dist_df <- data.frame(
   pat = c(), smp1 = c(), smp2 = c(), clonal_dist = c(),
-  td = c()
+  td = c(), pd = c()
 )
 for (pat in unique(clones_annotation$patient)) {
   smps <- unique(clones_annotation[clones_annotation$patient == pat, ]$sample)
@@ -57,8 +64,11 @@ for (pat in unique(clones_annotation$patient)) {
     clonal_dist <- get_dist(s1, s2, clones_annotation)
     if (s1 %in% rownames(td_mat) & s2 %in% rownames(td_mat)) {
       td <- td_mat[rownames(td_mat) == s1, colnames(td_mat) == s2]
+      pd <- abs(clones_annotation$purity[clones_annotation$sample == s1] -
+        clones_annotation$purity[clones_annotation$sample == s2])
     } else {
       td <- NA
+      pd <- NA
     }
 
     clonal_dist_df <- rbind(
@@ -66,7 +76,7 @@ for (pat in unique(clones_annotation$patient)) {
       data.frame(
         pat = pat, smp1 = s1, smp2 = s2,
         clonal_dist = clonal_dist,
-        td = td
+        td = td, pd = pd
       )
     )
   }
@@ -85,7 +95,16 @@ p <- violin_plot(
 
 save_ggplot(p, file.path(FIG_DIR, "Fig2C_td_clonaldist"), w = 75, h = 45)
 # Run LME to control for inclusion mulitple pairs from same patient
-run_lme(
+summary(
+  run_lme(
   "clonal_dist", "td", "pat",
   clonal_dist_df[!is.na(clonal_dist_df$td), ]
+  )
+)
+
+summary(
+  lme(td ~ pd + clonal_dist,
+    random = ~ 1 | pat,
+    data = clonal_dist_df[!is.na(clonal_dist_df$td) & !is.na(clonal_dist_df$pd), ]
+  )
 )
